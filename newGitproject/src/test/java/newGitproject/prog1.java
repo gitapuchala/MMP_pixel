f73245d51e6ad0c5c06ba65ce021ee5fdfc8141f
gitapuchala <gitapuchala@gmail.com>
2025-06-04 17:29:59 -0400

    Update prog1.java
    added text today

--- newGitproject/src/test/java/newGitproject/prog1.java
+++ newGitproject/src/test/java/newGitproject/prog1.java
@@ -4,7 +4,7 @@ public class prog1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("new changes");
+		System.out.println("new changes made today");
 	}
 
 }
